65f7284b77d330dd551673118478519c6d6b5547
chmisa <chmisa@node1.ftrace2.contools-pg0.utah.cloudlab.us>
2018-08-12 00:47:44 -0600

    Removed tcpdumping, added metadata collection, and sequence of arguments to run.sh

--- run.sh
+++ run.sh
@@ -22,7 +22,11 @@ B="----------------"
 NATIVE_PING_CMD="$(pwd)/iputils/ping"
 CONTAINER_PING_CMD="/iputils/ping"
 
-PING_ARGS="-D -i 0.5 -s 56" # arguments to hand to each invocation of ping
+# Sequence of arguments to hand to each invocation of ping
+declare -A PING_ARG_SEQ=(
+  ["i0.5_s56"]="-D -i 0.5 -s 56" 
+)
+
 PING_WAIT_CMD="sleep 600" # command to wait for ping measurement
 PAUSE_CMD="sleep 5"     # command to wait in between doing things
 
@@ -35,6 +39,25 @@ TRACE_ARGS="-e *sendto -e *recvmsg --date"
 # Argument to add to tcpdump invocations
 TCPDUMP_ARGS="-i eno1d1 icmp or icmp6"
 
+# Experiment book keeping
+DATE_TAG=`date +%Y%m%d%H%M%S`
+META_DATA="Metadata"
+
+#
+# Experiment Start
+#
+
+echo $B Gathering metadata $B
+
+mkdir $DATE_TAG
+cd $DATE_TAG
+
+# Get some basic meta-data
+echo "uname -a -> $(uname -a)" >> $META_DATA
+echo "docker -v -> $(docker -v)" >> $META_DATA
+echo "lsb_release -a -> $(lsb_release -a)" >> $META_DATA
+echo "sudo lshw -> $(sudo lshw)" >> $META_DATA
+
 # Start ping container as service
 docker run --rm -itd \
   --name=$PING_CONTAINER_NAME \
@@ -43,98 +66,77 @@ docker run --rm -itd \
 echo $B Started $PING_CONTAINER_NAME $B
 
 # START RUNS LOOP
+for file_sfx in ${!PING_ARG_SEQ[@]}
+do
+  PING_ARGS=${PING_ARG_SEQ[$file_sfx]}
 
-# # # # # # # # # # # # # # # 
-# Native procedure
-# # # # # # # # # # # # # # # 
-echo $B Running native $B
+  # # # # # # # # # # # # # # # 
+  # Native procedure
+  # # # # # # # # # # # # # # # 
+  echo $B Running native $B
 
-# Start ping
-$NATIVE_PING_CMD $PING_ARGS $TARGET_IPV4 > v4_native_${TARGET_IPV4}.ping &
-PING_PID=$!
-echo "  running ping with pid: $PING_PID"
-$PAUSE_CMD
+  # Start ping
+  $NATIVE_PING_CMD $PING_ARGS $TARGET_IPV4 > v4_native_${TARGET_IPV4}_${file_sfx}.ping &
+  PING_PID=$!
+  echo "  running ping with pid: $PING_PID"
+  $PAUSE_CMD
 
-# Start tcpdump
-tcpdump -w v4_native_${TARGET_IPV4}.pcap $TCPDUMP_ARGS &
-TCPDUMP_PID=$!
-echo "  running tcpdump with pid: $TCPDUMP_PID"
-$PAUSE_CMD
+  # Start ftrace
+  trace-cmd record $TRACE_ARGS -P $PING_PID \
+   -o v4_native_${TARGET_IPV4}_${file_sfx}.dat &
+  TRACE_PID=$!
+  echo "  running trace-cmd record with pid: $TRACE_PID"
 
-# Start ftrace
-trace-cmd record $TRACE_ARGS -P $PING_PID \
- -o v4_native_${TARGET_IPV4}.dat &
-TRACE_PID=$!
-echo "  running trace-cmd record with pid: $TRACE_PID"
+  # Let the data collect
+  $PING_WAIT_CMD
 
-# Let the data collect
-$PING_WAIT_CMD
+  # Stop ftrace
+  kill -INT $TRACE_PID
+  echo "  killed trace-cmd record"
+  $PAUSE_CMD
 
-# Stop ftrace
-kill -INT $TRACE_PID
-echo "  killed trace-cmd record"
-$PAUSE_CMD
+  # Stop ping
+  kill -INT $PING_PID
+  echo "  killed ping"
+  $PAUSE_CMD
 
-# Stop tcpdump
-kill -INT $TCPDUMP_PID
-echo "  killed tcpdump"
-$PAUSE_CMD
+  # # # # # # # # # # # # # # # 
+  # Container procedure
+  # # # # # # # # # # # # # # # 
+  echo $B Running container $B
 
-# Stop ping
-kill -INT $PING_PID
-echo "  killed ping"
-$PAUSE_CMD
+  # Start ping
+  docker exec $PING_CONTAINER_NAME $CONTAINER_PING_CMD $PING_ARGS $TARGET_IPV4 > v4_container_${TARGET_IPV4}_${file_sfx}.ping &
+  sleep 2
+  PING_PID=`ps -e | grep ping | sed -E "s/ *([0-9]+) .*/\1/"`
+  echo "  running ping with pid: $PING_PID"
+  $PAUSE_CMD
 
-# # # # # # # # # # # # # # # 
-# Container procedure
-# # # # # # # # # # # # # # # 
-echo $B Running container $B
+  # Start ftrace
+  trace-cmd record $TRACE_ARGS -P $PING_PID \
+    -o v4_container_${TARGET_IPV4}_${file_sfx}.dat &
+  TRACE_PID=$!
+  echo "  running trace-cmd record with pid: $TRACE_PID"
 
-# Start ping
-docker exec $PING_CONTAINER_NAME $CONTAINER_PING_CMD $PING_ARGS $TARGET_IPV4 > v4_container_${TARGET_IPV4}.ping &
-sleep 2
-PING_PID=`ps -e | grep ping | sed -E "s/ *([0-9]+) .*/\1/"`
-echo "  running ping with pid: $PING_PID"
-$PAUSE_CMD
+  # Let the data accumulate
+  $PING_WAIT_CMD
 
-# Start tcpdump
-tcpdump -w v4_container_${TARGET_IPV4}.pcap $TCPDUMP_ARGS &
-TCPDUMP_PID=$!
-echo "  running tcpdump with pid: $TCPDUMP_PID"
-$PAUSE_CMD
+  # Stop ftrace
+  kill -INT $TRACE_PID
+  echo "  killed trace-cmd record"
+  $PAUSE_CMD
 
-# Start ftrace
-trace-cmd record $TRACE_ARGS -P $PING_PID \
-  -o v4_container_${TARGET_IPV4}.dat &
-TRACE_PID=$!
-echo "  running trace-cmd record with pid: $TRACE_PID"
+  # Stop ping
+  kill -INT $PING_PID
+  echo "  killed ping"
+  $PAUSE_CMD
 
-# Let the data accumulate
-$PING_WAIT_CMD
+  # Convert to canonical text forms
+  echo $B Dumping to text $B
+  trace-cmd report -t -i v4_native_${TARGET_IPV4}_${file_sfx}.dat > v4_native_${TARGET_IPV4}_${file_sfx}.ftrace
+  trace-cmd report -t -i v4_container_${TARGET_IPV4}_${file_sfx}.dat > v4_container_${TARGET_IPV4}_${file_sfx}.ftrace
 
-# Stop ftrace
-kill -INT $TRACE_PID
-echo "  killed trace-cmd record"
-$PAUSE_CMD
-
-# Stop tcpdump
-kill -INT $TCPDUMP_PID
-echo "  killed tcpdump"
-$PAUSE_CMD
-
-# Stop ping
-kill -INT $PING_PID
-echo "  killed ping"
-$PAUSE_CMD
-
-# Convert to canonical text forms
-echo $B Dumping to text $B
-tcpdump -tt -n -r v4_native_${TARGET_IPV4}.pcap > v4_native_${TARGET_IPV4}.tcpdump
-trace-cmd report -t -i v4_native_${TARGET_IPV4}.dat > v4_native_${TARGET_IPV4}.ftrace
-tcpdump -tt -n -r v4_container_${TARGET_IPV4}.pcap > v4_container_${TARGET_IPV4}.tcpdump
-trace-cmd report -t -i v4_container_${TARGET_IPV4}.dat > v4_container_${TARGET_IPV4}.ftrace
-
-# END RUNS LOOP
+done # END RUNS LOOP
 
 docker stop $PING_CONTAINER_NAME
 echo $B Stoped $PING_CONTAINER_NAME $B
